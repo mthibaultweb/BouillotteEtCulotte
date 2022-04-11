ef823e215dd5e985b7b088c731958aa0d3c56fa2
flavie25 <flavie.dussaillant@gmail.com>
2022-04-11 09:34:49 +0200

    navbar

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,19 +1,23 @@
 let header = document.querySelector('.navbar');
 let navbarBrand = document.querySelector('.navbar-brand');
+let homepage = document.querySelector('body.homepage');
+
+if (homepage)
 
 function logo() {
     if (window.scrollY == 0) {
         let navbarToggler = document.querySelector('.navbar-toggler');
-        console.log(navbarToggler.ariaExpanded);
         if (navbarToggler.ariaExpanded) {
             navbarBrand.classList.toggle('solid');
-            console.log("woek");
         }
     }
 }
 
 window.addEventListener('scroll', function() {
     let windowPosition = window.scrollY > 0;
+    if (homepage) {
+        navbarBrand.classList.toggle('solid');
+    }
     header.classList.toggle('scrolling-active', windowPosition);
     navbarBrand.classList.toggle('solid', windowPosition);
 })
